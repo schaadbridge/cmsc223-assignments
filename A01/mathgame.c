a60950469170d151c604f98f60764e3d97a3a42a
Bridge <bschaad@haverford.edu>
2023-01-23 19:58:23 -0500

    Set seed for different numbers in each run.

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -2,27 +2,30 @@
  * mathgame.c
  * Author: Bridge Schaad 
  * Implements a math game
- * Created: 1/22/2023
+ * Created: 1/23/2023
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
   int num_rounds, curr_rounds;/**num_rounds: number of rounds
-	      corr_rounds: number of correct rounds */
+	     			 curr_rounds: number of correct rounds */
   printf("Welcome to Math Game!\n");
   printf("How many rounds do you want to play? ");
-  scanf("%d", &num_rounds);/**not sure if this will work, double check with notes!*/
+  scanf("%d", &num_rounds)
 
 
 /**Figure out how to generate two random digits 1-10*/
   for (int i = 0; i < num_rounds; i++) {
 	  int given_ans;
-	  int random1 = rand() % 10;
+	  time_t t;
+	  srand((unsigned) time(&t)); /** initialize a random number generator */
+	  int random1 = rand() % 10;  /**generate random number 1-10 */
 	  int random2 = rand() % 10;
-	  printf("%d + %d = ? ", random1, random2);
-	  scanf("%d", &given_ans); /**again, not sure. check.*/
+ 	  printf("%d + %d = ? ", random1, random2);
+	  scanf("%d", &given_ans);
 
 	  if (given_ans == random1+random2) {
 		curr_rounds++;
