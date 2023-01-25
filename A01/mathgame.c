01c117a5108aeb8a58204f7ef30f963a2c60cd00
Bridge <bschaad@haverford.edu>
2023-01-24 20:14:34 -0500

    fix valgrind issue

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -10,24 +10,25 @@
 #include <time.h>
 
 int main() {
-  int num_rounds, corr_rounds;/**num_rounds: number of rounds
+  int num_rounds = 0;
+  int corr_rounds = 0;/**num_rounds: number of rounds
 	     			 corr_rounds: number of correct rounds */
   printf("Welcome to Math Game!\n");
   printf("How many rounds do you want to play? ");
   scanf("%d", &num_rounds);
 
 
-/**Figure out how to generate two random digits 1-10*/
-  for (int i = 0; i < num_rounds; i++) {
-	  int given_ans;
-	  time_t t;
-	  srand((unsigned) time(&t)); /** initialize a random number generator */
+  int i = 0;
+  for (i; i < num_rounds; i++) {
+	  int given_ans = 0;
+	  srand(time(NULL)); /** initialize a random number generator */
 	  int random1 = rand() % 10;  /**generate random number 1-10 */
 	  int random2 = rand() % 10;
+
  	  printf("%d + %d = ? ", random1, random2);
 	  scanf("%d", &given_ans);
 
-	  if (given_ans == random1+random2) {
+	  if (given_ans == (random1 + random2)) {
 		corr_rounds++;
 		printf("Correct!\n");
 	  }
@@ -35,6 +36,6 @@ int main() {
 		printf("Incorrect :(\n");
 	  }
   }
-  printf("You answered %d/%d correctly.\n", curr_rounds, num_rounds);
+  printf("You answered %d/%d correctly.\n", corr_rounds, num_rounds);
   return 0;
 }
