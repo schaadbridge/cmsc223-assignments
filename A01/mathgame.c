03b74ac9c92de8c60357b3d56ce5d1be46517119
Bridge <bschaad@haverford.edu>
2023-01-24 22:49:59 -0500

    implement style standards.

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -10,9 +10,9 @@
 #include <time.h>
 
 int main() {
-  int num_rounds = 0;
-  int corr_rounds = 0;/**num_rounds: number of rounds
-	     			 corr_rounds: number of correct rounds */
+  int num_rounds = 0; /** num_rounds: number of rounds */
+  int corr_rounds = 0; /** corr_rounds: number of correct rounds */
+
   printf("Welcome to Math Game!\n");
   printf("How many rounds do you want to play? ");
   scanf("%d", &num_rounds);
@@ -20,22 +20,23 @@ int main() {
 
   int i = 0;
   for (i; i < num_rounds; i++) {
-	  int given_ans = 0;
-	  srand(time(NULL)); /** initialize a random number generator */
-	  int random1 = rand() % 10;  /**generate random number 1-10 */
-	  int random2 = rand() % 10;
+    int given_ans = 0;
+    srand(time(NULL)); /** initialize a random number generator */
+    int random1 = rand() % 10; /**generate random number 1-10 */
+    int random2 = rand() % 10;
 
- 	  printf("%d + %d = ? ", random1, random2);
-	  scanf("%d", &given_ans);
+    printf("%d + %d = ? ", random1, random2);
+    scanf("%d", &given_ans);
 
-	  if (given_ans == (random1 + random2)) {
-		corr_rounds++;
-		printf("Correct!\n");
-	  }
-	  else {
-		printf("Incorrect :(\n");
-	  }
+    if (given_ans == (random1 + random2)) {
+      corr_rounds++;
+      printf("Correct!\n");
+    }
+    else {
+      printf("Incorrect :(\n");
+    }
   }
+
   printf("You answered %d/%d correctly.\n", corr_rounds, num_rounds);
   return 0;
 }
