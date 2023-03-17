43e4ce94eb93fdbc45d53fe91f6732e1cb38dfd4
Bridge <bschaad@haverford.edu>
2023-03-17 15:16:08 -0400

    Part 1

--- A08/warmup.c
+++ A08/warmup.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main() {
-  return 0;
+    pid_t pid;
+    printf("%d] A0\n", getpid());
+    
+    pid = fork();
+    if (pid != 0) {
+        printf("%d] B0\n", getpid()); 
+        
+        pid = fork();
+        if (pid != 0) {
+            printf("%d] C0\n", getpid());
+        }
+        else {
+            printf("%d] C1\n", getpid());
+        }
+    }
+    else {
+        printf("%d] B1\n", getpid());
+    }
+    printf("%d] Bye\n", getpid());
+    return 0;
 }
